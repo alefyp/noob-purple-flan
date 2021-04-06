3bfccbe6ab658bbcec29efaf6e058e6a932aca5c
alefyp <alejndraprz@gmail.com>
2021-04-05 19:43:48 -0500

    init flan

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'turtle-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'noob-purple-flan';
+}
